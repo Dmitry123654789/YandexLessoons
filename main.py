53af834707e68fb2b08f26e1346161fdd30dd126
Dmitry <pokemon2708@yandex.ru>
2025-05-18 16:04:12 +0300

    Угадай-ка город

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from random import choice
 
-import pygame
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 from support import *
+from ui_file import Ui_MainWindow
 
 CITY = open('country.txt', "r", encoding="utf-8").read().split(', ')  # 1116 городов
 
@@ -21,27 +23,44 @@ def save_image(city):
         file.write(get_response_map(ll_toponym, spn_toponym))
 
 
-def draw_pygame_map(map_file):
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                city = choice(CITY)
-                save_image(city)
-                screen.blit(pygame.image.load(map_file), (0, 0))
-                pygame.display.flip()
-                # print(city)
-    pygame.quit()
-    os.remove(map_file)
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.player = -1
+        self.points = [0, 0]
+        self.city_draw = choice(CITY)
+        self.answer_btn.clicked.connect(self.check_answer)
+        self.set_setting()
+
+    def check_answer(self):
+        if self.answer_line_edit.text().lower() == self.city_draw.lower():
+            self.points[self.player] += 1
+            self.set_setting()
+        else:
+            if self.player == 1:
+                city = self.city_draw
+                self.set_setting()
+                self.no_answer.setText(f'Это был(а): {city}')
+            else:
+                self.player += 1
+                self.label_2.setText(f"Игрок номер {self.player + 1} ваш ответ: ")
+
+    def set_setting(self):
+        self.points_1.setText(f'Игрок номер 1: {str(self.points[0])} очка(ов)')
+        self.points_2.setText(f'Игрок номер 2: {str(self.points[1])} очка(ов)')
+        self.player = 0
+        self.city_draw = choice(CITY)
+        save_image(self.city_draw)
+        self.label_2.setText(f"Игрок номер {self.player + 1} ваш ответ: ")
+        self.label_map.setPixmap(QPixmap('map.png'))
+        os.remove('map.png')
+        self.no_answer.setText('')
+        # print(self.city_draw)
 
 
 if __name__ == '__main__':
-    save_image(choice(CITY))
-    draw_pygame_map("map.png")
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
